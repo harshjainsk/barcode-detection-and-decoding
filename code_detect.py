b24b529d0eda7ea1ef55a51eae8772a53e4c0947
harshjainsk <harshjainsk531@gmail.com>
2023-03-14 22:52:00 +0530

    converted detected image from RGB to BGR

--- code_detect.py
+++ code_detect.py
@@ -8,7 +8,6 @@ from PIL import Image
 
 
 
-
 def create_barcode():
 
     bar_code = barcode.get_barcode_class('code128')
@@ -22,35 +21,39 @@ def create_barcode():
 
 def barcode_decoder(image):
 
+    # image comes in numpy array and then decoded    
     decoded_info = decode(image)
-
-    print(decoded_info)
+    return decoded_info
 
 
 def detect_and_crop_img(image_path):
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'best.pt', force_reload = True)
 
+    # loading model and detecting barcode
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'best.pt', force_reload = True)
     results = model(image_path)
-    print(results.xyxy)
+
+    # print(results.xyxy)
+
+    # extracting bounding box coordinates
     bbox = results.xyxy[0][0]
-    print(bbox)
+    # print(bbox)
     xmin = int(bbox[0])
     ymin = int(bbox[1])
     xmax = int(bbox[2])
     ymax = int(bbox[3])
 
+    # reading image and cropping
     image = cv2.imread(image_path)
 
     cropped_image = image[ymin:ymax, xmin:xmax]
-    gray_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-    bright_img = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,181,15)
-    cv2.imshow("bright image" , bright_img)
-    cv2.waitKey(0)
-    return barcode_decoder(cropped_image)
+
+    # converting the RGB output image to BGR image
+    gray_image = cv2.cvtColor(cropped_image, cv2.COLOR_RGB2BGR)
+
+    return barcode_decoder(gray_image)
 
 
-# barcode_decoder('output.jpg')
 
-img = detect_and_crop_img('trial2.jpg')
+decoded_info = detect_and_crop_img('images\\trial2.jpg')
 
-print(img)
+print(decoded_info)
